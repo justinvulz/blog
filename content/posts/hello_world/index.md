3f2ae8c98af2a1d994976141e03554529fca4814
justinChen <justin.chen@thinktrace.com>
2026-01-29 14:50:16 +0800

    init

---
+++ content/posts/hello_world/index.md
@@ -0,0 +1,7 @@
++++
+date = '2026-01-29T14:45:20+08:00'
+draft = false
+title = 'Posts hello world'
++++
+i
+hi
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Rust flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # or whatever vers
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux"; # your version
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          hugo
+        ]; # whatever you need
+      };
+    };
+}
